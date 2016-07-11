6dd38756209537db8e9cf844b4b5fffefb9b2ab4
Richard Barnes <rbarnes@umn.edu>
2016-07-11 11:51:32 -0600

    Updated with architecture note

--- src/install.libs.R
+++ src/install.libs.R
@@ -3,7 +3,7 @@ if(WINDOWS)
   execs <- c("apps/dggrid/dggrid.exe")
 
 if ( any(file.exists(execs)) ) {
-  dest <- file.path(R_PACKAGE_DIR, 'bin') #paste0('bin', R_ARCH)
+  dest <- file.path(R_PACKAGE_DIR, 'bin')  #paste0('bin', R_ARCH) or Sys.info()['machine'] might be useful
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(execs, dest, overwrite = TRUE)
 }
