4b4a90d04b24f6a07b812e30b9ccd2e928c51093
Richard Barnes <rbarnes@umn.edu>
2016-07-11 11:46:50 -0600

    Output exe to a more predictable location. Warning: may interfere with multi-arch builds

--- src/install.libs.R
+++ src/install.libs.R
@@ -3,7 +3,7 @@ if(WINDOWS)
   execs <- c("apps/dggrid/dggrid.exe")
 
 if ( any(file.exists(execs)) ) {
-  dest <- file.path(R_PACKAGE_DIR,  paste0('bin', R_ARCH))
+  dest <- file.path(R_PACKAGE_DIR, 'bin') #paste0('bin', R_ARCH)
   dir.create(dest, recursive = TRUE, showWarnings = FALSE)
   file.copy(execs, dest, overwrite = TRUE)
 }
