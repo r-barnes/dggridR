9ed8a8077d2b04528e4d5a142a6d1fb4395bad8b
Richard Barnes <rbarnes@umn.edu>
2017-08-06 23:32:05 -0400

    Eliminated tests that no longer apply

--- tests/testthat/test_grid_construction.R
+++
@@ -1,8 +0,0 @@
-
-grid_types <- c("ISEA3H", "ISEA4H", "ISEA43H", "ISEA4T", "ISEA4D", "FULLER3H", "FULLER4H", "FULLER43H", "FULLER4T", "FULLER4D")
-
-test_that("All grids construct properly", {
-  for(gt in grid_types){
-     expect_error( dgconstruct(type=gt, spacing=1000, metric=FALSE, resround='down'), NA)
-  }
-})
